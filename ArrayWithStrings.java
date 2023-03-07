e3743316d912c9da3d8750b3cf55b068f932a364
Abraham Vallejos-Soto <125098951+AVSbeats@users.noreply.github.com>
2023-03-07 07:47:29 -0500

    Update ArrayWithStrings.java

--- ArrayWithStrings.java
+++ ArrayWithStrings.java
@@ -1,13 +1,15 @@
 public class ArrayWithStrings {
     public static void main(String[] args) {
 
-        String[] saiyans = new String[5];
+        String[] saiyans = new String[7];
 
         saiyans[0] = "Goku";
         saiyans[1] = "Vegeta";
         saiyans[2] = "Gohan";
         saiyans[3] = "Trunks";
         saiyans[4] = "Goten";
+        saiyans[5] = "Broly";
+        saiyans[6] = "Bardock";
 
         System.out.println("The saiyans are: ");
 
